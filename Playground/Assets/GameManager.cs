e7d210c1c82bb68abd26d5b9394143efd09f3a37
Nantarat Channoi <taeza38007@hotmail.com>
2016-05-12 14:42:40 +0700

    Add Project

---
+++ Playground/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.GetKeyDown (KeyCode.Space)) 
+//		{
+//			SceneManager.LoadScene ("level2");
+//		}
+	}
+
+}
---
+++ Playground/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		speed = 25.0f;
+
+	}
+
+
+	void FixedUpdate(){
+
+
+		float moveLeft = Input.GetAxis ("Horizontal");
+		float moveUp = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveLeft,0.0f,moveUp);
+
+		rb.AddForce (movement*speed);
+
+
+
+	}
+
+}
